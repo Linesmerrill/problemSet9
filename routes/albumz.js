e8e1f9d1bbf65e1106e001e29191a23ddc1d863e
Your Name <localadmin@compozed-tss-10.mobile.asu.edu>
2016-06-21 16:34:38 -0700

    search working

--- routes/albumz.js
+++ routes/albumz.js
@@ -9,6 +9,26 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/', function(req, res, next) {
+  var obj = {
+  }
+
+  if(req.body.artist !== ''){
+    obj.artist = req.body.artist;
+  }
+  if(req.body.album !== ''){
+    obj.album = req.body.album;
+  }
+  if(req.body.genre !== ''){
+    obj.genre = req.body.genre;
+  }
+
+
+  db.get('albums').find(obj, function(err, albums) {
+    res.render('albumz/index', { albums: albums });
+  });
+});
+
 router.get('/new', function(req, res) {
   res.render('albumz/new');
 });
@@ -21,6 +41,7 @@ router.put('/:id', function(req, res){
 });
 
 router.post('/', function(req, res) {
+  console.log(req.body)
   db.get('albums').insert({genre: req.body.genre, artist: req.body.artist, album: req.body.album, rating: req.body.rating, isExplicit: req.body.explicit}, function(err, data) {
     res.redirect('/albumz');
   });
